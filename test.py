b4ac3ff6e87a19cb2932821f867c90117bf19fcb
armel <deghislain@gmail.com>
2024-04-29 10:06:41 -0300

    initial comit

---
+++ test.py
@@ -0,0 +1,40 @@
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.agents import load_tools, initialize_agent
+from langchain.agents import AgentType
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import tool
+from datetime import date
+import os
+from colorama import Fore
+
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", openai_api_key=os.getenv('OPEN_AI_KEY'))
+agent = create_python_agent(
+    llm,
+    tool=PythonREPLTool(),
+    verbose=True
+)
+tools = load_tools(["llm-math"], llm=llm)
+
+
+@tool
+def time(text: str) -> str:
+    """Returns todays date, use this for any \
+    questions related to knowing todays date. \
+    The input should always be an empty string, \
+    and this function will always return todays \
+    date - any date mathmatics should occur \
+    outside this function."""
+
+    return str(date.today())
+
+
+agent = initialize_agent(
+    tools + [time],
+    llm,
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True,
+    verbose=True)
+print(Fore.BLUE + "Hi I am your personal assistant how can I help you?:")
+prompt = str(input())
+agent.run(prompt)
